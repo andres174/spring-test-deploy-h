7cdca9b6b8bd502e4e20cfd4132367831909d6fb
andres174 <zambrasda17@gmail.com>
2024-06-16 14:13:05 -0500

    Deploy 1

---
+++ src/main/java/com/curso/springboot/ob/srpingdeploy/SrpingDeployApplication.java
@@ -0,0 +1,13 @@
+package com.curso.springboot.ob.srpingdeploy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SrpingDeployApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SrpingDeployApplication.class, args);
+    }
+
+}
